739fc9c1f07580626f7c6006e965a139d516eb54
MadhusudanDas <msdas7476@gmail.com>
2026-08-30 00:10:46 +0530

    better auth api route created

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,8 +2,12 @@
 import { betterAuth } from "better-auth" 
 import { prismaAdapter } from "better-auth/adapters/prisma"
 import db from "./db"
- 
+
+
 export const auth = betterAuth({
+
+    baseURL: process.env.BETTER_AUTH_URL, 
+
     database: 
             prismaAdapter(db , {
                 provider: "postgresql"
