751cea1529ec7f38e092161c9cebc72ee43196ee
MadhusudanDas <msdas7476@gmail.com>
2026-08-29 23:13:22 +0530

    update database connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,17 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
-const db = globalThis.prisma || new PrismaClient({ });
+const connectionString = process.env.DATABASE_URL!;
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+const db = globalThis.prisma || new PrismaClient({
+  adapter,
+  log: ["error", "info", "query", "warn"],
+});
 
 if (process.env.NODE_ENV === "development") {
   globalThis.prisma = db;
